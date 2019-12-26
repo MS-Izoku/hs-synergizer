aada6a098affe0cd6ff765b69abba04180072ad2
Martin Spicuzza <spicuzza157@gmail.com>
2019-12-26 15:01:31 -0600

    added routes for future functionality, based on previous ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :cards, only: [:index]
-  get '/standard-cards', to: 'cards#standard_cards'
-  get '/wild-cards', to: 'cards#wild_cards'
+  get '/cards/standard', to: 'cards#standard_cards' , as: 'standard_cards'
+  get '/standard/by-mechanic/:id', to: 'cards#standard_cards_by_mechanic', as: 'standard_by_mechanic'
+  get '/standard/by-tribe/:id', to: 'cards#standard_cards_by_tribe', as: 'standard_by_tribe'
+  get '/standard/spells', to: 'cards#standard_spells', as: 'standard_spells'
 
-  get '/standard-cards/by-mechanic/:id', to: 'cards#standard_cards_by_mechanic', as: 'standard_by_mechanic'
-  get '/standard-cards/by-tribe/:id', to: 'cards#standard_cards_by_tribe', as: 'standard_by_tribe'
-  get '/standard-cards/spells', to: 'cards#standard_spells', as: 'standard_spells'
+  get '/cards/wild', to: 'cards#wild_cards' , as: 'wild_cards'
+  get '/wild/by-mechanic/:id', to: 'cards#wild_cards_by_mechanic', as: 'wild_by_mechanic'
+  get '/wild/by-tribe/:id', to: 'cards#wild_cards_by_tribe', as: 'wild_by_tribe'
+  get '/wild/spells', to: 'cards#wild_spells', as: 'wild_spells'
 
-  get '/cards/:id', to: 'cards#show'
-
-  resources :mechanics, only: %i[index show]
   resources :cards , only: [:index , :show]
-  resources :deck
+  resources :mechanics, only: %i[index show]
+  resources :keyword , only: [:index , :show]
+  
+  resources :deck , except: [:index , :edit , :new]
+  resources :tribe , except: [:new , :create , :update , :edit]
+
 end
