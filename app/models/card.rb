24743be4d71fb7c4a3f5f15daf555d0c398930c2
Martin Spicuzza <spicuzza157@gmail.com>
2019-12-28 11:51:30 -0600

    starting the parsing method for key phrases

--- app/models/card.rb
+++ app/models/card.rb
@@ -160,59 +160,247 @@ class Card < ApplicationRecord
      'after this minion survives damage', 'at the start your turn', 'your minions with', 'casts a random', 'plays a random',
      'start the game', 'if your deck is empty', 'if you have no', 'if your hand has no', 'go dormant', 'the first', 'your first',
      'your cards that summon minions', "if you're holding a dragon", 'if you played an elemental last turn', 'if you have 10 mana crystals',
-     'if your hand has no']
+     'summon' , "restore" , "reduce the cost"]
   end
 
   def parse_key_phrases
     all_keywords = {
-      remaining_plain_text: ''
+      remaining_plain_text: plain_text.downcase,
+      keywords: {
+
+      }
     }
 
-    text = plain_text
-    Card.key_phrases.each do |phrase|
-      next unless text.include?(phrase)
+    Card.key_phrases.collect do |phrase|
+      next unless all_keywords[:remaining_plain_text].include?(phrase)
 
-      if phrase == 'if your deck has no duplicates'
-        # CardMechanic.create(card_id: self.id , mechanic_id: Mechanic.find_or_create_by(name: ""))
-        CardMechanic.create(card_id: id, mechanic_id: Mechanic.find_or_create_by(name: 'singleton'))
-      elsif phrase == 'return it to life'
-        # CardMechanic.create(card_id: self.id , mechanic_id: Mechanic.find_or_create_by(name: "ressurect").id)
-        make_card_mechanic('ressurect')
-      elsif phrase == 'if your deck is empty'
-        CardMechanic.create(card_id: id, mechanic_id: Mechanic.find_or_create_by(name: 'empty deck').id)
-      elsif phrase == "if you're holding a dragon"
-        tribe = Tribe.find_by(name: 'Dragon')
-        CardMechanic.create(card_id: id, mechanic_id: Mechanic.find_or_create_by(name: 'tribal', tribal_synergy_id: tribe.id))
-      elsif phrase == "your opponent's cards"
-        # CardMechanic.create(card_id: self.id , mechanic_id: Mechanic.find_or_create_by(name: "disruption"))
-        make_card_mechanic('disruption')
-      elsif phrase == 'if you played an elemental last turn'
-        tribe = Tribe.find_by(name: 'elemental')
-        CardMechanic.create(card_id: id, mechanic_id: Mechanic.find_or_create_by(name: 'tribal', tribal_synergy_id: tribe.id))
-      elsif phrase == 'give it'
-        # some kind of buff
-        nil
-      elsif phrase == 'if you have no'
-        # x cost minions
-        # spells
+      p phrase
+      case phrase
+      when 'if your deck has no duplicates'
+        puts 'SINGLETON FOUND'
+        all_keywords[:keywords]['singleton'] = 1
+        next
+      when "your opponent's cards"
+        puts 'DISRUPTION FOUND'
+        if all_keywords[:keywords]['disruption'].nil? 
+          all_keywords[:keywords]['disruption'] = 0 
+        else
+           all_keywords[:keywords]['disruption'] += 1
+        end
+        next
+      when 'at the start of your turn'
+        puts 'START OF TURN FOUND'
+        next
+      when '50% chance to'
+        puts 'OGRE FOUND'
+        next
+      when 'if your board is full of'
+        puts 'MOGU CULTIST FOUND'
+        next
+      when 'whenever you play'
+        puts 'ON CARD PLAY FOUND'
+        next
+      when 'after you'
+        puts 'AFTER PLAYER ACTION FOUND'
+        next
+      when 'each player'
+        puts 'EACH-PLAYER FOUND'
+        next
+      when 'reveal a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'for each'
+        puts 'SINGLETON FOUND'
+        next
+      when "if you're holding a spell that costs (5) or more"
+        puts 'SINGLETON FOUND'
+        next
+      when 'if you have unspent mana at the end of your turn'
+        puts 'SINGLETON FOUND'
+        next
+      when 'if you control'
+        puts 'SINGLETON FOUND'
+        next
+      when 'it costs'
+        puts 'SINGLETON FOUND'
+        next
+      when 'after you play'
+        puts 'SINGLETON FOUND'
+        next
+      when 'after you cast'
+        puts 'SINGLETON FOUND'
+        next
+      when 'targets chosen randomly'
+        puts 'SINGLETON FOUND'
+        next
+      when 'split among'
+        puts 'SINGLETON FOUND'
+        next
+      when "set a minion's"
+        puts 'SINGLETON FOUND'
+        next
+      when  'from your deck'
+        puts 'SINGLETON FOUND'
+        next
+      when  'hero power'
+        puts 'SINGLETON FOUND'
+        next
+      when  'return it to life'
+        puts 'SINGLETON FOUND'
+        next
+      when 'after you summon a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'return a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'change each'
+        puts 'SINGLETON FOUND'
+        next
+      when 'equip a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'casts when drawn'
+        puts 'SINGLETON FOUND'
+        next
+      when 'summons when'
+        puts 'SINGLETON FOUND'
+        next
+      when "while you're"
+        puts 'SINGLETON FOUND'
+        next
+      when 'your hero takes damage'
+        puts 'SINGLETON FOUND'
+        next
+      when 'discard'
+        puts 'SINGLETON FOUND'
+        next
+      when 'for the rest of the game'
+        puts 'SINGLETON FOUND'
+        next
+      when 'whenever your hero attacks'
+        puts 'SINGLETON FOUND'
+        next
+      when 'choose a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'if you have'
+        puts 'SINGLETON FOUND'
+        next
+      when 'spell damage'
+        puts 'SINGLETON FOUND'
+        next
+      when 'your spells cost'
+        puts 'SINGLETON FOUND'
+        next
+      when 'your opponents spells cost'
+        puts 'SINGLETON FOUND'
+        next
+      when 'copy a'
+        puts 'SINGLETON FOUND'
+        next
+      when 'whenever this minion'
+        puts 'SINGLETON FOUND'
+        next
+      when "can't be targeted by spells or hero powers"
+        puts 'SINGLETON FOUND'
+        next
+      when 'until your next turn'
+        puts 'SINGLETON FOUND'
+        next
+      when 'take an extra turn'
+        puts 'SINGLETON FOUND'
+        next
+      when "fill each player's"
+        puts 'SINGLETON FOUND'
+        next
+      when 'after this minion survives damage'
+        puts 'SURVIVAL FOUND'
+        next
+      when 'at the start your turn'
+        puts 'START-OF-TURN FOUND'
+        next
+      when 'your minions with'
+        puts 'MINION BUFF FOUND'
+        next
+      when 'casts a random'
+        puts 'RANDOM-CAST FOUND'
+        next
+      when 'plays a random'
+        puts 'RANDOM-PLAY FOUND'
+        next
+      when 'start the game'
+        puts 'START-OF-GAME FOUND'
+        next
+      when 'if your deck is empty'
+        puts 'NOMI FOUND'
+        next
+      when 'if you have no'
+        puts 'IF-YOU-HAVE-NO FOUND'
+        next
+      when 'if your hand has no'
+        puts 'IF-YOUR-HAND-HAS-NO FOUND'
         # minions
-        nil
-      elsif phrase == 'your spells cost'
-        # go through the phrase to find the "your spells cost", then parse the integer in ()
-        CardMechanic.create(card_id: id, mechanic_id: Mechanic.find_or_create_by(name: 'cost change').id, cost_change: 1)
-      elsif phrase == 'at the end of your turn'
-        make_card_mechanic('end of turn')
-      elsif phrase == 'from your deck'
-          
+        # spells
+        # tribal?
+        next
+      when 'go dormant'
+        puts 'DORMANT FOUND'
+        next
+      when 'the first'
+        puts 'THE-FIRST FOUND'
+        # spell
+        # minion
+        next
+      when 'your first'
+        puts 'YOUR-FIRST FOUND'
+        next
+      when 'your cards that summon minions'
+        puts 'KHADGAR FOUND'
+        if all_keywords[:keywords]["summon"].nil?
+          all_keywords[:keywords]["summon"] = 1
+        else
+          all_keywords[:keywords]["summon"] += 1
+        end
+        next
+      when "if you're holding a dragon"
+        puts 'DRAGON HOLDING FOUND'
+        all_keywords[:keywords]["dragon"] = 1
+        next
+      when 'if you played an elemental last turn'
+        puts 'ELEMENTAL CHAIN FOUND'
+        all_keywords[:keywords]
+        next
+      when 'if you have 10 mana crystals'
+        puts 'OMEGA FOUND'
+        all_keywords[:keywords]["omega"] = 1
+        next
+      when 'summon'
+        puts 'SUMMONING FOUND'
+        # summon a
+        # summon x-many
+        # summon specific minion
+        next
+      when 'restore'
+        puts "HEALTH-RESTORE FOUND"
+        # parse int from the string , then find the target type
+        next
+      when 'reduce the cost'
+        puts "CARD REDUCTION"
+        reduction_amount = 1 # get the actual value from a string parse
+        all_keywords[:keywords]["cost-reduction"] = reduction_amount
+        next
       else
-        target_mechanic = Mechanic.find_by(name: phrase)
+        puts "Phrase --#{phrase}-- not found"
+        next
       end
-
-      text.slice!(phrase)
+      all_keywords[:remaining_plain_text].slice!(phrase)
     end
-    all_keywords[:remaining_plain_text] = text
 
     all_keywords
+  end
+
+  def quick_split_string(inp_string , splitter_string)
   end
 
   def make_card_mechanic(mechanic_name)
