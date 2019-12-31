abefc16161c085acd792cc29f5cfe389df585a28
Martin Spicuzza <spicuzza157@gmail.com>
2019-12-30 22:02:32 -0600

    final commit for the night, working on adding password requirements and other user information

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,20 @@
 class User < ApplicationRecord
   has_secure_password
 
-    # need to handle error validation for this end, might do that on the front-end
-    PASSWORD_REQUIREMENTS = /\A
-    (?=.{8,50}) # length-check , might be unrealistically long
-    (?=.*\d) # digit-check
-    (?=.*[a-z]) # lower-case check
-    (?=.*[A-Z]) # capital check
-    (?=.[[:^alnum:]]) # symbol check
-    /x.freeze
+  # need to handle error validation for this end
+  PASSWORD_REQUIREMENTS = /\A
+  (?=.{8,50}) # length-check , might be unrealistically long
+  (?=.*\d) # digit-check
+  (?=.*[a-z]) # lower-case check
+  (?=.*[A-Z]) # capital check
+  (?=.[[:^alnum:]]) # symbol check
+  /x.freeze
 
-  validates_format_of :email, with: Devise.email_regexp
-  validates :username, uniqueness: true, length: { in: 6..20 }
-  validates :password, length: { in: 6..20 }, format: PASSWORD_REQUIREMENTS
+  def get_password_error # WIP, needs to take specific error into account for string parsing
+    'Password must be between 8-50 characters and contain at least 1 number, 1 upper-case letter, 1 lower-case letter, and 1 special character'
+  end
 
-
+  validates_format_of :email, with: Devise.email_regexp, uniqueness: { message: 'Email already exists, did you forget your password?' }
+  validates :username, uniqueness: { message: 'Username already taken, please try again' }, length: { in: 6..20, message: 'Username must be between 6 and 20 characters' }
+  validates :password, format: PASSWORD_REQUIREMENTS
 end
