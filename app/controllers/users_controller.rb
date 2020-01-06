1c53863d0ee61229e57c934f9e76854b42bf30ba
Martin Spicuzza <spicuzza157@gmail.com>
2020-01-06 10:48:14 -0600

    added start of boilerplate users_controller for auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
-    def profile
+    def create
+        user = User.create(user_creation_params)
+        if user.valid?
+            render json: UserSerializer.new(user)
+        else
+            render json: {message: "Error creating user" , error: user.errors} , status: 400
+        end
     end
 
-    def create
+    def profile
+        render json: UserSerializer.new(current_user) , status: :accepted
     end
 
     def delete
+        
     end
 
     private
     def user_params
+    end
+
+    def user_creation_params
+        params.require(:user).permit(:username , :password , :email)
     end
 end
