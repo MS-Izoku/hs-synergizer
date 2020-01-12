9527eefcfec4d67388e94aa6d49943ce2a43b558
Martin Spicuzza <spicuzza157@gmail.com>
2020-01-11 19:38:18 -0600

    modified the mechanics model

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -4,20 +4,25 @@ class Mechanic < ApplicationRecord
   has_many :card_mechanics
   has_many :cards, through: :card_mechanics
 
-  #has_many :deck_mechanics
-  #has_many :decks, through: :deck_mechanics
-
   def self.names(show_in_console = false)
     temp = []
 
     Mechanic.all.each do |mechanic|
       temp.push(mechanic.name.downcase)
-      p mechanic.name if show_in_console
+      puts mechanic.name if show_in_console
     end
     temp.sort!
   end
 
-  def self.cards_with_mechanic(mechanic_name)
-    Mechanic.find_by(name: mechanic_name).cards
+  def self.cards_with_mechanic(mechanic_name, standard)
+    mechanic_id = Mechanic.find_by(name: mechanic_name).id
+    sets = CardSet.where(standard: standard)
+    p sets
+    p "SETS"
+    cards = []
+
+    
+
+    cards
   end
 end
