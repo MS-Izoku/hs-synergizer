ea1bf76f1a429ee10eeca2d4de9b9ce7afdf946c
Martin Spicuzza <spicuzza157@gmail.com>
2020-01-28 12:46:59 -0600

    working on making card rendering better, as well as changing up what gets rendered generally (HoF and removed cards should not show up)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,26 +4,24 @@ class CardsController < ApplicationController
   def index
     cards = Card.joins(:card_set).where(collectable: true).where.not(card_sets: { name: ['NYI', 'Hero Classes', 'Token'] })
     cards = cards.paginate(page: params[:page])
-    #render json: { cards: CardSerializer.new(cards), page_count: cards.total_pages }
+    # render json: { cards: CardSerializer.new(cards), page_count: cards.total_pages }
     render json: create_paginated_json(cards)
   end
 
   def wild_cards
     cards = Card.wild_cards.paginate(page: params[:page], per_page: index_pagination_count(1))
-    render json: CardSerializer.new(cards)
+    render json: create_paginated_json(cards)
   end
 
   def standard_cards
     # cards = Card.standard_cards.paginate(page: params[:page], per_page: index_pagination_count(1))
     cards = Card.standard_cards.paginate(page: params[:page])
-    render json: CardSerializer.new(cards)
+    render json: create_paginated_json(cards)
   end
 
   def standard_cards_by_mechanic
     cards = CardMechanic.where(mechanic_id: params[:id]).cards
-    # cards = cards.paginate(page: params[:page], per_page: index_pagination_count(20))
-    # render json: CardSerializer.new(cards)
-    render_if_cards_exist(cards, index_pagination_count(1))
+    create_paginated_json(cards)
   end
 
   def standard_spells
@@ -52,10 +50,18 @@ class CardsController < ApplicationController
   end
 
   def create_paginated_json(cards)
-    {
-      cards: CardSerializer.new(cards),
-      page_count: cards.total_pages,
-      page: params[:page]
-    }
+    if cards
+      {
+        cards: CardSerializer.new(cards),
+        page_count: cards.total_pages,
+        page: params[:page]
+      }
+    else
+      {
+        message: 'Internal Service Error finding Cards',
+        error: cards.error,
+        status: 404
+      }
+    end
   end
 end
--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -1,13 +1,62 @@
+# frozen_string_literal: true
+
 class CardSet < ApplicationRecord
-    has_many :cards
-    has_many :artists , through: :cards
+  has_many :cards
+  has_many :artists, through: :cards
 
-    def card_names
-        temp = []
-        self.cards.each do |card|
-            temp = temp.push(card.name)
-        end
-        temp
+  def card_names
+    temp = []
+    cards.each do |card|
+      temp = temp.push(card.name)
     end
-end
+    temp
+  end
 
+  def self.set_removed_cards
+    Card.where(img: nil).update_all(card_set_id: CardSet.find_or_create_by(name: 'Removed').id)
+    Card.where(name: Card.removed_cards).update(card_set_id: CardSet.where(name: 'Removed'))
+    Card.joins(:card_set).where(card_sets: { name: [
+                                  'NYI', 'Token', 'Debug', 'Hero Cards', 'Removed'
+                                ] }).delete_all
+  end
+
+  def self.move_cards_to_set(set_name, _card_names)
+    set = CardSet.find_by(name: set_name)
+    Card.where(name: cards).update(card_set_id: set.id)
+  end
+
+  def self.removed_cards
+    ['Absolution', 'Adrenaline Rush', 'Arcane Flux', "Assassin's Training",
+     'Auto-Pecker 4000', 'Axe of the Eclipse', 'Avatar of the Coin',
+     'Bethekk, the Panther', 'Boon of Elune', 'Captain Scaleblade',
+     'Cheerful Spirit', 'Combust', 'Dark Summoner', 'Darkspear Hunter',
+     'Death Wish', 'Demoralizing Roar', 'Devouring Ooze', 'Envenom',
+     'Etheral Zug', 'Faceless Zug', 'Fade', 'Firebeard Herald',
+     'Ghost', 'Goblin Zug', 'Grand Poet Ezzera', 'Greater Heal', 'Grimoire of Service',
+     'Half-dead Walrus', 'Hardboiled Constable', 'High Mezmerist Ezzera',
+     'Jadefire Satyr', 'Kobold Zug', 'Magma Shock', 'Mana Spring Totem',
+     'MEGA-BLAST!!!', 'Mental Collapse', 'Mindspike', 'Oldtown Beggar',
+     'Overmind Kangor', 'Ozumat', 'Power Cosmic', 'Prayer of Fortitude',
+     'Puddlejumper', 'Quest for Epic Loot', 'Refreshing Jolt', 'Relic of Hope',
+     'Retribution', 'Scrap Reacer', 'Sydnicate Spy', 'The Omnireaper',
+     'Training Blade', 'Volley', 'Witchy Zug', 'Worldflipper X-50']
+  end
+
+  def self.hall_of_fame
+    [
+      'Coldlight Oracle', 'Azure Drake', "Captain's Parrot", 'Molten Giant',
+      'Old Murk-Eye', 'Elite Tauren Chieftain', 'Geblin Mekkatorque', 'Genn Greymane',
+      'Sylvanas Windrunner', 'Ragnaros the Firelord', 'Baku the Mooneater',
+      'Chicken', 'Murloc', 'I Am Murloc', 'Power of the Horde', 'Rogues Do It...',
+      'Emboldener 3000', 'Homing Chicken', 'Poultryizer', 'Repair Bot',
+      'Naturalize', 'Gloom Stag', 'Ice Lance', 'Black Cat', 'Ice Block',
+      'Divine Favor', 'Mind Blast', 'Glitter Moth', 'Murkspark Eel',
+      'Conceal', 'Vanish', 'Power Overwhelming', 'Doomguard'
+    ]
+  end
+
+  def self._special_event_sets
+    set_names = ['Wild Event']
+    CardSet.where(name: set_names)
+  end
+end
